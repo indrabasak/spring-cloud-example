848d21d4ffff73a3deb954a4f621018960ef819b
Indra Basak <indra.basak1@gmail.com>
2017-09-25 14:32:10 -0700

    Fixed spring-boot-maven-plugin issues and added a few tests.

--- services/author-service/src/test/java/com/basaki/cloud/author/controller/AuthorControllerMockMvcTest.java
+++ services/author-service/src/test/java/com/basaki/cloud/author/controller/AuthorControllerMockMvcTest.java
@@ -1,26 +1,27 @@
 package com.basaki.cloud.author.controller;
 
 import com.basaki.cloud.author.boot.TestAuthorApplication;
-//import com.basaki.cloud.author.model.Author;
-//import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//import com.basaki.cloud.author.model.Author;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -49,17 +50,25 @@ public class AuthorControllerMockMvcTest {
                         "application/json;charset=UTF-8")).andDo(
                 MockMvcResultHandlers.print());
 
-//        String uri = "";
-//        ObjectMapper mapper = new ObjectMapper();
-//        Author author = new Author();
-//        author.setAuthor("indra");
-//        author.setBiography("hello there");
-//        mockMvc.perform(MockMvcRequestBuilders.post("/authors").with(user(
-//                "userAx").roles("AUTHOR_USER"))
-//                .content(mapper.writeValueAsString(author))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andDo(MockMvcResultHandlers.print());
+        MvcResult result = mockMvc.perform(get("/authors/1"))
+                //.with(user("userAx").roles("AUTHOR_USER")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(
+                        "application/json;charset=UTF-8")).andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        System.out.println(content);
+        //        String uri = "";
+        //        ObjectMapper mapper = new ObjectMapper();
+        //        Author author = new Author();
+        //        author.setAuthor("indra");
+        //        author.setBiography("hello there");
+        //        mockMvc.perform(MockMvcRequestBuilders.post("/authors").with(user(
+        //                "userAx").roles("AUTHOR_USER"))
+        //                .content(mapper.writeValueAsString(author))
+        //                .contentType(MediaType.APPLICATION_JSON)
+        //                .accept(MediaType.APPLICATION_JSON))
+        //                .andExpect(status().isOk())
+        //                .andDo(MockMvcResultHandlers.print());
     }
 }
